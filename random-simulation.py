9cd3edb45247860e161a13c37c784cd2649f5cbc
Tevin Mantock <tmantock@gmail.com>
2016-11-05 21:55:03 -0700

    flip plot

--- random-simulation.py
+++ random-simulation.py
@@ -134,4 +134,60 @@ def checkPascal(numTrials = 100000):
                 break
     print 'Probability of losing = ' + str(1.0 - yes/numTrials)
 
-checkPascal()
+#checkPascal()
+
+def flip(numFlips):
+    heads = 0
+    for i in range(numFlips):
+        if random.random() < 0.5:
+            heads += 1
+    return heads/float(numFlips)
+
+#print flip(100)
+
+##def flipSim(numFlipsPerTrial, numTrials):
+##    fracHeads = []
+##    for i in range(numTrials):
+##        fracHeads.append(flip(numFlipsPerTrial))
+##    mean = sum(fracHeads)/float(len(fracHeads))
+##    return (mean)
+
+def flipPlot(minExp, maxExp):
+    ratios = []
+    diffs = []
+    xAxis = []
+    for exp in range(minExp, maxExp + 1):
+        xAxis.append(2**exp)
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.random() < 0.5:
+                numHeads += 1
+        numTails = numFlips - numHeads
+        ratios.append(numHeads/float(numTails))
+        diffs.append(abs(numHeads - numTails))
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails')
+    pylab.plot(xAxis, diffs)
+    pylab.figure()
+    pylab.plot(xAxis, ratios)
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Heads/Tails')
+    pylab.figure()
+    pylab.title('Difference Between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(#Heads - #Tails')
+    pylab.plot(xAxis, diffs, 'bo')
+    pylab.semilogx()
+    pylab.semilogy()
+    pylab.figure()
+    pylab.plot(xAxis, ratios, 'bo')
+    pylab.title('Heads/Tails Ratios')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Heads/Tails')
+    pylab.semilogx()
+
+flipPlot(4, 20)
+pylab.show()
