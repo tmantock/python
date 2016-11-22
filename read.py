be12cec9774b0ab5c39b63fbd38f016a7823d70a
Tevin Mantock <tmantock@gmail.com>
2016-11-22 14:14:34 -0800

    able to join liked csv files to one combined file

--- read.py
+++ read.py
@@ -1,28 +1,48 @@
 import os
 #plan for restructuring for inheritance for different file types
-class CSVReader(object):
+class Write(object):
     def __init__(self, directory, filetype):
         self.directory = directory
         self.filetype = filetype
-        self.fileHeaders = []
-    def readFiles(self):
+    def readToWriteFiles(self):
+        for filename in os.listdir(str(self.directory)):
+            if filename.endswith('.' + str(self.filetype)):
+                file = open(str(self.directory) + filename, 'r')
+                output_file = open('output.txt','a')
+                for line in file.readlines():
+                    output_file.write(line)
+                file.close()
+                output_file.close()
+
+class WriteCSV(Write):
+    new_files = {}
+    file_loc = {}
+    file_number = 0;
+    numIters = 0
+    def __init__(self, directory, filetwype):
+        self.file_headers = set()
+        Write.__init__(self, directory, filetype)
+    def readToWriteFiles(self):
         for filename in os.listdir(str(self.directory)):
             if filename.endswith('.' + str(self.filetype)):
                 file = open(str(self.directory) + filename, 'r')
                 header_line = file.readline()
-                #incomplete portion
-                #purpose is to create a new file if the headers do not match for a csv
-                file_number = 1
-                if(len(self.header_line) > 0):
-                    if header_line in self.fileHeaders:
-                        file_number += 1
-                ###################################################################
-                of = open('output' + str(file_number) + '.txt', 'a')
-                self.fileHeaders.append(header_line)
+                if header_line in WriteCSV.new_files.values():
+                    output_file = open(WriteCSV.file_loc[header_line], 'a')
+                else:
+                    output_filename = 'output' + str(WriteCSV.file_number) + '.csv'
+                    WriteCSV.new_files[output_filename] = header_line
+                    WriteCSV.file_loc[header_line] = output_filename
+                    output_file = open(output_filename, 'a')
+                    output_file.write(header_line)
+                    WriteCSV.file_number += 1
                 for line in file.readlines():
-                    of.write(line)
+                    output_file.write(line)
+                output_file.write('\n')
                 file.close()
-                of.close()
+                output_file.close()
+                print filename + " has printed to " + output_filename + "."
+                WriteCSV.numIters += 1
 
 inUse = True
 
@@ -31,6 +51,10 @@ while inUse:
     directory = raw_input("Which directory would you like to use? ")
     filetype = raw_input("Which file type would you like to use? ")
 
-    reader = CSVReader(directory, filetype)
-    reader.readFiles()
+    if filetype == 'csv':
+        writer = WriteCSV(directory, filetype)
+        writer.readToWriteFiles()
+    else:
+        writer = Write(directory, filetype)
+        writer.readToWriteFiles()
     inUse = False
