8a1b91ad47a0b57f1dcd8ecbfb9b521fbdab22c0
Tevin Mantock <tmantock@gmail.com>
2016-11-21 17:50:22 -0800

    Create read.py
    read.py eventual file reader and writer for combining files

---
+++ read.py
@@ -0,0 +1,32 @@
+import os
+
+class CSVReader(object):
+    def __init__(self, directory, filetype):
+        self.directory = directory
+        self.filetype = filetype
+        self.fileHeaders = []
+    def readFiles(self):
+        for filename in os.listdir(str(self.directory)):
+            if filename.endswith('.' + str(self.filetype)):
+                file = open(str(self.directory) + filename, 'r')
+                header_line = file.readline()
+                file_number = 1
+                if(len(self.header_line) > 0):
+                    if header_line in self.header_line:
+                        file_number++
+                of = open('output' + str(file_number) + '.txt', 'w')
+                self.fileHeaders.append(header_line)
+                for line in file.readlines():
+                    of.write(line)
+                file.close()
+                of.close()
+
+inUse = True
+
+while inUse:
+    directory = raw_input("Which directory would you like to use? ")
+    filetype = raw_input("Which file type would you like to use? ")
+
+    reader = CSVReader(directory, filetype)
+    reader.readFiles()
+    inUse = False
