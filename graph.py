7948e0188585ea89406a5a9ddc0878bf80b2ac00
Tevin Mantock <tmantock@gmail.com>
2016-11-24 17:12:54 -0800

    dynamic programming shortesr point

--- graph.py
+++ graph.py
@@ -74,5 +74,151 @@ def test1(kind):
     print 'The graph:'
     print g
 
-test1(Digraph)
-test1(Graph)
+# test1(Digraph)
+# test1(Graph)
+
+numCalls = 0
+
+def shortestPath(graph, start, end, toPrint = False, visited = []):
+    global numCalls
+    numCalls += 1
+    if toPrint: #for debugging
+        print start, end
+    if not (graph.hasNode(start) and graph.hasNode(end)):
+        raise ValueError('Start or end not in graph.')
+    path = [str(start)]
+    if start == end:
+        return path
+    shortest = None
+    for node in graph.childrenOf(start):
+        if (str(node) not in visited): #avoid cycles
+            visited = visited + [str(node)] #new list
+            newPath = shortestPath(graph, node, end, toPrint, visited)
+            if newPath == None:
+                continue
+            if (shortest == None or len(newPath) < len(shortest)):
+                shortest = newPath
+    if shortest != None:
+        path = path + shortest
+    else:
+        path = None
+    return path
+
+def test2(kind, toPrint = False):
+    nodes = []
+    for name in range(10):
+        nodes.append(Node(str(name)))
+    g = kind()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[2]))
+    g.addEdge(Edge(nodes[2],nodes[3]))
+    g.addEdge(Edge(nodes[3],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[5]))
+    g.addEdge(Edge(nodes[0],nodes[2]))
+    g.addEdge(Edge(nodes[1],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[0]))
+    g.addEdge(Edge(nodes[4],nodes[0]))
+    print 'The graph:'
+    print g
+    shortest = shortestPath(g, nodes[0], nodes[4], toPrint)
+    print 'The shortest path:'
+    print shortest
+
+# test2(Graph)
+# test2(Digraph)
+
+def bigTest1(kind, numNodes = 25, numEdges = 200):
+    nodes = []
+    for name in range(numNodes):
+        nodes.append(Node(str(name)))
+    g = kind()
+    for n in nodes:
+        g.addNode(n)
+    for e in range(numEdges):
+        src = nodes[random.choice(range(0, len(nodes)))]
+        dest = nodes[random.choice(range(0, len(nodes)))]
+        g.addEdge(Edge(src, dest))
+    print g
+    print shortestPath(g, nodes[0], nodes[4])
+
+# bigTest1(Digraph)
+#
+# test2(Graph, toPrint = True)
+
+def dpShortestPath(graph, start, end, visited = [], memo = {}):
+    global numCalls
+    numCalls += 1
+    if not (graph.hasNode(start) and graph.hasNode(end)):
+        raise ValueError('Start or end not in graph.')
+    path = [str(start)]
+    if start == end:
+        return path
+    shortest = None
+    for node in graph.childrenOf(start):
+        if (str(node) not in visited):
+            visited = visited + [str(node)]
+            try:
+                newPath = memo[node, end]
+            except:
+                newPath = dpShortestPath(graph, node, end,
+                                         visited, memo)
+            if newPath == None:
+                continue
+            if (shortest == None or len(newPath) < len(shortest)):
+                shortest = newPath
+                memo[node, end] = newPath
+    if shortest != None:
+        path = path + shortest
+    else:
+        path = None
+    return path
+
+def test3(kind):
+    nodes = []
+    for name in range(10):
+        nodes.append(Node(str(name)))
+    g = kind()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[2]))
+    g.addEdge(Edge(nodes[2],nodes[3]))
+    g.addEdge(Edge(nodes[3],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[5]))
+    g.addEdge(Edge(nodes[0],nodes[2]))
+    g.addEdge(Edge(nodes[1],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[0]))
+    g.addEdge(Edge(nodes[4],nodes[0]))
+    print 'The graph:'
+    print g
+    shortest = shortestPath(g, nodes[0], nodes[4])
+    print 'The shortest path:'
+    print shortest
+    shortest = dpShortestPath(g, nodes[0], nodes[4])
+    print 'The shortest path:'
+    print shortest
+# test3(Digraph)
+
+def bigTest2(kind, numNodes = 25, numEdges = 200):
+    global numCalls
+    nodes = []
+    for name in range(numNodes):
+        nodes.append(Node(str(name)))
+    g = kind()
+    for n in nodes:
+        g.addNode(n)
+    for e in range(numEdges):
+        src = nodes[random.choice(range(0, len(nodes)))]
+        dest = nodes[random.choice(range(0, len(nodes)))]
+        g.addEdge(Edge(src, dest))
+    print g
+    numCalls = 0
+    print shortestPath(g, nodes[0], nodes[4])
+    print 'Number of calls to shortest path =', numCalls
+    numCalls = 0
+    print dpShortestPath(g, nodes[0], nodes[4])
+    print 'Number of calls to dp shortest path =', numCalls
+
+bigTest2(Digraph)
