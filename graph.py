def0ac7f1d0fc9c25bf36150eca6224e23c081b3
Tevin Mantock <tmantock@gmail.com>
2016-11-24 16:30:54 -0800

    added graphy.py

---
+++ graph.py
@@ -0,0 +1,78 @@
+import pylab, random, string, copy
+
+class Node(object):
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest, weight = 0):
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        return str(self.src) + '->' + str(self.dest)
+
+class Digraph(object):
+    def __init__(self):
+        self.nodes = set([])
+        self.edges = {}
+    def addNode(self, node):
+        if node.getName() in self.nodes:
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.add(node)
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.nodes
+    def __str__(self):
+        res = ''
+        for k in self.edges:
+            for d in self.edges[k]:
+                res = res + str(k) + '->' + str(d) + '\n'
+        return res[:-1]
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+def test1(kind):
+    nodes = []
+    for name in range(10):
+        nodes.append(Node(str(name)))
+    g = kind()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[2]))
+    g.addEdge(Edge(nodes[2],nodes[3]))
+    g.addEdge(Edge(nodes[3],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[5]))
+    g.addEdge(Edge(nodes[0],nodes[2]))
+    g.addEdge(Edge(nodes[1],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[0]))
+    g.addEdge(Edge(nodes[4],nodes[0]))
+    print 'The graph:'
+    print g
+
+test1(Digraph)
+test1(Graph)
