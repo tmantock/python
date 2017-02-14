c55756cf9c5057d137dd00a0898e3ccb4433b9fe
Tevin Mantock <tmantock@gmail.com>
2017-02-14 11:44:38 -0800

    Create primes.py

---
+++ primes.py
@@ -0,0 +1,25 @@
+import math
+
+def primelist(limit):
+    primes = []
+
+    for x in range(limit):
+        prime = True
+
+        if(x < 2):
+            continue
+        #An integer is prime if it is not divisible by any prime less than or equal to its square root
+        sqrt = math.floor(math.sqrt(x))
+
+        for i in range(2, sqrt + 1):
+            if(x % i == 0):
+                prime = False
+
+        if(prime == True):
+            primes.append(x)
+
+    return primes
+
+primes = primelist(100)
+
+print(primes)
