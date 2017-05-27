bc75515aa8fabca9a59f991135e4cbe6e7e6be4d
Tevin Mantock <tmantock@gmail.com>
2017-05-27 13:31:48 -0700

    binary search

--- search.py
+++ search.py
@@ -14,9 +14,7 @@ def binary_search(haystack, needle):
         h = haystack[mid:]
     elif needle < haystack[mid]:
         h = haystack[:mid]
-
     return binary_search(h, needle)
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20]
-
-print binary_search(arr, 18)
+arr = range(1,100000)
+print binary_search(arr, 96)
