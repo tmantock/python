f3049ea3e53aa635749bdccb46e6ff55372a458f
Tevin Mantock <tmantock@gmail.com>
2017-05-28 16:13:18 -0700

    while loop test

--- search.py
+++ search.py
@@ -16,5 +16,23 @@ def binary_search(haystack, needle):
         h = haystack[:mid]
     return binary_search(h, needle)
 
+def binary_search_while(haystack, needle):
+    if needle > haystack[-1] or needle < haystack[0]:
+        return "Out of Bounds"
+    go = True
+
+    while go:
+        mid = len(haystack) / 2
+
+        if needle == haystack[mid]:
+            go = False
+            return haystack[mid]
+        elif needle > haystack[mid]:
+            haystack = haystack[mid:]
+        elif needle < haystack[mid]:
+            haystack = haystack[:mid]
+
 arr = range(1,100000)
-print binary_search(arr, 96)
+
+print binary_search_while(arr, 55)
+print binary_search(arr, 55)
