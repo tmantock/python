aba1ceaaa705b7566127f3573295848f3cdbf0ab
Tevin Mantock <tmantock@gmail.com>
2017-05-29 23:01:54 -0700

    Queue data structure

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 #Python Programming
 
-This repository began as practice of the Python programming as I learned the syntax of the language and includes a Tic-Tac-Toe and Blackjack game.
-
-It has since been become the repository that I upload code that I have learned from the MIT Open Courseware lectures for Introduction to Computer Science, since the course utilizes the python language.
+Learning and practicing Computer Science Fundamentals with Python
---
+++ queueStructure.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+    def getData(self):
+        return self.data
+    def getNext(self):
+        return self.next
+    def setNext(self, node):
+        self.next = node
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.count = 0
+        self.max = 10
+
+    def enqueue(self, n):
+        if self.count == self.max:
+            print "Max reached"
+            return
+
+        self.count += 1
+
+        nd = Node(n)
+
+        if self.first is None:
+            self.first = nd
+            return
+
+        if self.first is not None and self.last is None:
+            self.first.setNext(nd)
+            self.last = nd
+            return
+
+        self.last.setNext(nd)
+        self.last = nd
+
+    def dequeue(self):
+        if self.first is None:
+            print "Que is empty"
+            return
+
+        n = self.first.getNext()
+        self.first.setNext(None)
+        self.first = n
+        self.count -= 1
+
+    def peek(self):
+        return self.first.getData()
+
+    def offer(self):
+        if self.max != self.count:
+            return True
+        return False
+
+    def traverse(self):
+        if self.first is None:
+            return
+
+        n = self.first
+
+        while n:
+            print n.getData()
+            n = n.getNext()
+
+def testQue():
+    que = Queue()
+    que.enqueue("Jenny")
+    que.enqueue("James")
+    que.enqueue("Jeremy")
+    que.enqueue("Claire")
+    que.enqueue("John")
+    que.traverse()
+    print "\n \n"
+    que.dequeue()
+    que.traverse()
+    print "\n \n"
+    que.dequeue()
+    que.dequeue()
+    que.dequeue()
+    que.dequeue()
+    que.dequeue()
+
+    if que.offer():
+        que.enqueue("Troy")
+
+    print "\n \n"
+    que.traverse()
+
+testQue()
