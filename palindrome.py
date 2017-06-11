a83537d1cc4c3a22137e343ee7d9219d0d71abe2
Tevin Mantock <tmantock@gmail.com>
2017-06-11 15:34:32 -0700

    palindrome testing

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,4 @@
 ##Recurssion
-
 def toChars(s):
     import string
     s = string.lower(s)
@@ -15,11 +14,25 @@ def isPal(s):
     else:
         return s[0] == s[-1] and isPal(s[1:-1])
 
-def isPalindrome(s):
+def IsPalindrome(s):
     """Returns True if s is a palindrome and False otherwise"""
     return isPal(toChars(s))
 
-print isPalindrome('Guttag')
-print isPalindrome('Guttug')
-print isPalindrome('Able was I ere I saw Elba')
-print isPalindrome('Are we not drawn onward, we few, drawn onward to new era?')
+# print isPalindrome('Guttag')
+# print isPalindrome('Guttug')
+# print isPalindrome('Able was I ere I saw Elba')
+# print isPalindrome('Are we not drawn onward, we few, drawn onward to new era?')
+
+root = Node("g")
+n1 = Node("u")
+root.setNext(n1)
+n2 = Node("t")
+n1.setNext(n2)
+n3 = Node("t")
+n2.setNext(n3)
+n4 = Node("u")
+n3.setNext(n4)
+n5 = Node("g")
+n4.setNext(n5)
+
+isPalindrome(root)
