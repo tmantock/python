24d471837981285f06acd1fd92178ee83278834e
Tevin Mantock <tmantock@gmail.com>
2017-09-20 22:55:28 -0700

    DFS and BFS

--- tree.py
+++ tree.py
@@ -11,7 +11,7 @@ class Node:
     def getData(self):
         return self.data
     def insert(self, data):
-        if self.data <= data:
+        if self.data >= data:
             if self.left is None:
                 self.left = Node(data, 'Left')
             else:
@@ -28,7 +28,7 @@ class Node:
             return self.left.find(data)
         elif self.data > data and self.right is not None:
             return self.right.find(data)
-        
+
         return False
     def printInOrder(self):
         if self.left is not None:
@@ -48,6 +48,27 @@ class Node:
         if self.right is not None:
             self.right.printPreOrder()
         print self
+    def BFS(self):
+        queue = [self]
+        while queue:
+            root = queue.pop(0)
+            if root.left:
+                queue.append(root.left)
+            if root.right:
+                queue.append(root.right)
+            print root
+    def DFS(self):
+        nodes = []
+        stack = [self]
+        while stack:
+            root = stack.pop(0)
+            nodes.append(root)
+            if root.left:
+                stack.insert(0, root.left)
+            if root.right:
+                stack.insert(0, root.right)
+        for n in nodes:
+            print n
     def __str__(self):
         return "( " + str(self.position) + ", " + str(self.data) + ")"
 
@@ -55,6 +76,8 @@ root = Node(5)
 root.insert(4)
 root.insert(7)
 root.insert(3)
+root.insert(5)
+root.insert(6)
 print root.find(8)
 print "```````````````````"
 root.printInOrder()
@@ -62,3 +85,7 @@ print "```````````````````"
 root.printPreOrder()
 print "```````````````````"
 root.printPostOrder()
+print "---------------------"
+root.BFS()
+print "*********************"
+root.DFS()
